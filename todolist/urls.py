2b2cd9bcf376537d8b4d32a81768be6ada065650
Vu Nguyen <vanguyen106@gmail.com>
2021-03-21 18:10:00 -0700

    urls

--- todolist/urls.py
+++ todolist/urls.py
@@ -18,4 +18,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('todo/', include('todo.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
