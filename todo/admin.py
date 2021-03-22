2e103c2db9e7cc954d6f6b64914192e3c859e1e1
Vu Nguyen <vanguyen106@gmail.com>
2021-03-21 19:25:37 -0700

    4 done

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Todo, Suggestions
 # Register your models here.
+admin.site.register(Todo)
+admin.site.register(Suggestions)
+
+
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+#producttype, product, review
+
+class Todo(models.Model):
+    what=models.CharField(max_length=255)
+    where=models.TextField()
+    when=models.DateField()
+    time=models.TimeField()
+    why=models.TextField(null=True, blank=True)
+    how=models.TextField(null=True, blank=True)
+    userid=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.what
+
+    class Meta:
+        db_table='To Do List'
+
+class Suggestions(models.Model):
+    suggestwhat=models.CharField(max_length=255)
+    suggestwhere=models.TextField()
+    suggestwhen=models.DateField()
+    suggesttime=models.TimeField()
+    suggestwhy=models.TextField(null=True, blank=True)
+    suggesthow=models.TextField(null=True, blank=True)
+    userid=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.suggestwhat
+
+    class Meta:
+        db_table='Suggestions'
