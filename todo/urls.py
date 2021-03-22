78423cd85a9722af989e46ad682966d6c7563ea4
Vu Nguyen <vanguyen106@gmail.com>
2021-03-21 19:58:41 -0700

    5

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('todolist/', views.todo, name='todolist'),
+    path('suggestion/', views.suggestion, name='suggestion'),
+
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Todo, Suggestions
 
 # Create your views here.
 def index (request):
     return render(request, 'todo/index.html')
+
+def todo(request):
+    todo_list=Todo.objects.all()
+    return render(request, 'todo/todolist.html', {'todo_list': todo_list})
+
+def suggestion(request):
+    suggestion_list=Suggestions.objects.all()
+    return render(request, 'todo/suggestion.html', {'suggestion_list': suggestion_list})
